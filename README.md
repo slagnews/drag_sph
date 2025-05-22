0cc4b94059b7339ec31183bd908dff3d11b599ae
slagnews <swinkelsn@gmail.com>
2025-05-22 15:27:58 +0200

    Added interactions and pressure calculation

--- README.md
+++ README.md
@@ -1,23 +1,16 @@
 # Project 3
+The central idea of Smoothed Particle Hydrodynamics (SPH) simulations is to approximate continuous materials by a set of particles, of which the properties are interpolated over the space in between them. This is done using a kernel function $W(\mathbf{r}-\mathbf{r}_\mathrm{pt},h)$ where $\mathbf{r}$ is the position at which the property is being evaluated, and $\mathbf{r}_\mathrm{pt}$ the position of the particle of which the contribution is being assessed, and $h$ is a scale factor which is a measure for how spread out the properties are.
 
-For the final project of Computational Physics we encourage you to define your
-own project!
+In an abstract sense, any property $A(\mathbf{r})$ (this could be the density), can be approximated by sampling at different positions $mathbf{r'}$ and spreading these out using such a kernel function, such that we end up with an interpolated approximation
+$$
+A_\mathrm{int}(\mathbf{r})=\int_\mathcal{V}A(\mathbf{r}')W(\mathbf{r}-\mathbf{r}',h)\mathrm{d}\mathbf{r}'
+$$
 
-The only requirement we ask for is that it should be a simulation of a
-physics-related project (but we consider physics-related rather broad, and e.g.
-simulation of biological systems or an engineering problem are also absolutely
-ok).
-We do advise you though to start from a project where there is some existing literature.
-in this way you have a starting point as well as something to compare your simulation to
-for validation.
-
-We encourage you to discuss your project idea with us during the time of class, 
-or remotely via the planning issue #1. 
-
-In any case, you need to fill in a short plan (a few lines) together with a
-reference to literature in the planning issue, *and* have it agreed by us before
-May 21 (i.e. latest two weeks before the presentation).
-
-If you have problems to come up with a good project, we can provide you with
-proven project ideas. But we first want you to try coming up with your own project!
-Having designed your own project will also give a small bonus for the grade.
+The acceleration of a particle $i$ is given by
+$$
+\mathbf{a}_i=-\sum_{j}m_j \left(\frac{P(\mathbf{r}_i)}{\rho(\mathbf{r}_i)^2}+\frac{P(\mathbf{r}_j)}{\rho(\mathbf{r}_j)^2}\right)\nabla W_{ij}
+$$
+(see Monaghan eq. 3.8). with $\nabla W_{ij}$ the gradient of the kernel function of $j$ at the position of $i$, so
+$$
+\nabla W_{ij}=\frac{\mathbf{r}_i-\mathbf{r}_j}{|\mathbf{r}_i-\mathbf{r}_j|}\frac{\mathrm{d}}{\mathrm{d}r}W(r,h)|_{r=|\mathbf{r}_i-\mathbf{r}_j|}
+$$
--- sph.py
+++ sph.py
@@ -2,20 +2,57 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-def kernel(dx,h):
+np.random.seed(0)
+
+def kernel(r,h):
     """Compute the kernel function for SPH.
     Arguments:
-        x: Position of the particle.
-        x0: Position of the reference particle.
+        r: distance
         h: Smoothing length.
     Returns:
         Kernel value.
     """
-    return 1/(2*np.pi*h**2)*np.exp(-0.5*(dx/h)**2)
+    return 1/(2*np.pi*h**2)*np.exp(-0.5*(r/h)**2)
 
+def deriv_kernel(r,h):
+    return -r/(2*np.pi*h**4)*np.exp(-0.5*(r/h)**2)
 
-def density(positions, h, L, grid_size=100):
-    """Calculates the density of particles on grid.
+def relative_positions(positions, L):
+    """Calculates relative positions and distances between particles.
+    Arguments:
+        positions (np.ndarray): The positions of the particles in cartesian space
+        L (float): The size of the simulation space
+    Returns:
+        rel_positions (np.ndarray): Relative positions of particles
+        rel_distances (np.ndarray): The distance between particles
+    """
+    
+    # Calculate NxNxD array for N atoms in D dimensions storing relative positions:
+    rel_positions = positions[np.newaxis,:,:] - positions[:,np.newaxis,:]
+
+    # Normalize relative positions according to periodic boundary conditions:
+    rel_positions = (rel_positions + L/2)%L - L/2
+
+    # Calculate distances from normalized distances:
+    rel_distances = np.sqrt(np.sum(rel_positions**2, axis=2))
+
+    return rel_positions, rel_distances
+
+def get_densities(positions, masses, L, h):
+    """Calculates the densities at the positions of all particles.
+    Arguments:
+        positions (np.ndarray): The particle positions in carthesian space
+        masses (np.ndarray):  The particle masses
+        L (float): the size of the simulation space
+        h (float) the kernel size
+    Returns:
+        (np.ndarray): densities at particle positions
+    """
+    rel_pos, rel_dist = relative_positions(positions, L)
+    return np.sum(masses*kernel(rel_dist, h), axis=1)
+
+def get_density_grid(positions, masses, L, h, grid_size=100):
+    """Calculates the densities on a grid
     Arguments:
         positions: 2D numpy array of shape (no_particles, 2) containing the x and y coordinates of the particles.
         h: Smoothing length.
@@ -32,15 +69,44 @@ def density(positions, h, L, grid_size=100):
     X,Y = np.meshgrid(x,y)
 
     # Apply periodic boundary conditions to distances
-    dx = (X[:,:,None]-positions[:,0])#%L
-    dx = dx - L * np.round(dx / L)
+    dx = (X[:,:,None]-positions[:,0])
+    dx = (dx+L/2)%L - L/2
+    #dy = dx - L*np.round(dx/L)
     dy = (Y[:,:,None]-positions[:,1])#%L
-    dy = dy - L * np.round(dy / L)
+    dy = (dy+L/2)%L - L/2
+    #dy = dy - L * np.round(dy / L)
+
+    distances = np.sqrt(dx**2+dy**2)
 
     # Calculate density
-    density = np.sum(kernel(np.sqrt(dx**2 + dy**2), h), axis=2)
+    density = np.sum(masses*kernel(distances, h), axis=2)
     return x,y,density
 
+def tait_pressure(rho, c_s, rho0, gamma):
+    B = c_s**2*rho0/gamma
+    return B*((rho/rho0)**gamma-1)
+
+def get_pressures(densities, c_s, rho0):
+    """Calculates the pressures at all particle positions"""
+    pressures = tait_pressure(densities, c_s, rho0, 7)
+    return pressures
+
+def get_pressure_grid(density_grid, c_s, rho0):
+    """Calculates the pressures on a grid"""
+    pressure_grid = tait_pressure(density_grid, c_s, rho0, 7)
+    return x,y,pressure_grid
+
+def accelerations(positions, masses, L, h, c_s, rho0):
+    rel_pos, distances = relative_positions(positions, L)
+    accelerations = np.zeros_like(positions)
+    densities = get_densities(positions, masses, L, h)
+    pressures = get_pressures(densities, c_s, rho0)
+
+    distances2 = np.where(distances==0, np.inf, distances)
+    nablaW = deriv_kernel(distances, h)[:,:,None]*-rel_pos/distances2[:,:,None]
+    for i in range(len(positions)):
+        accelerations[i] = -np.sum((masses*(pressures/densities**2+pressures[i]/densities[i]**2))[:,None]*nablaW[i],axis=0)
+    return accelerations
 
 def animate_particles(positions, L, no_frames):
     """Animate the positions of particles in a 2D space.
@@ -57,7 +123,7 @@ def animate_particles(positions, L, no_frames):
     ax.set_ylabel("$y$")
     ax.set_aspect("equal")
     scatter, = ax.plot(positions[0,:,0], positions[0,:,1], linestyle="None", marker="o", markersize=1, color="red")
-    x,y,rho = density(positions[0], 0.5, L)
+    x,y,rho = get_density_grid(positions[0], masses, L, h)
     mesh = ax.pcolor(x,y,rho, cmap="viridis")
 
     def update(frame):
@@ -65,7 +131,7 @@ def animate_particles(positions, L, no_frames):
         print(f"frame {frame}")
         scatter.set_xdata(positions[frame,:,0])
         scatter.set_ydata(positions[frame,:,1])
-        x,y,rho = density(positions[frame], 0.5, L)
+        x,y,rho = get_density_grid(positions[frame], masses, L, h)
         mesh.set_array(rho.ravel())
         return scatter,mesh
 
@@ -73,7 +139,7 @@ def animate_particles(positions, L, no_frames):
     plt.show()
     ani.save("animation.mp4", fps=30, extra_args=['-vcodec', 'libx264'], dpi=300)
 
-def integrate(initial_positions, initial_velocities, L, dt, no_steps):
+def integrate(initial_positions, initial_velocities, masses, L, h, c_s, rho0, dt, no_steps):
     """Integrate the positions and velocities of particles in a 2D space.
     Arguments:
         initial_positions: 2D numpy array of shape (no_particles, 2) containing the initial x and y coordinates of the particles.
@@ -93,25 +159,45 @@ def integrate(initial_positions, initial_velocities, L, dt, no_steps):
     for i in range(1, no_steps):
         positions[i] = positions[i-1] + velocities[i-1] * dt
         positions[i] %= L  # Apply periodic boundary conditions
-        velocities[i] = velocities[i-1]  # Assuming constant velocity for simplicity
-
+        velocities[i] = velocities[i-1] + dt*accelerations(positions[i], masses, L, h, c_s, rho0) # Assuming constant velocity for simplicity
+        print(f"{(i+1)/no_steps*100:.0f}% done")
     return positions, velocities
 
 if __name__ == "__main__":
-    no_particles = 1000
+    no_particles = 100
     no_steps = 100
-    L = 10
+    L = 3
+    h = 0.5
     v0 = 0.1
-    dt = 0.1
-    initial_positions = np.random.random(size=(no_particles,2))*L
-    initial_velocities = np.random.normal(size=(no_particles,2))*v0
+    dt = 0.01
+    c_s = 10
+    rho0 = 10
+    masses = np.array([1]*no_particles)
 
-    positions, velocities = integrate(initial_positions, initial_velocities, L, dt, no_steps)
-    fig, ax = plt.subplots()
-    x,y,rho = density(positions[0], 0.5, L)
-    ax.pcolor(x,y,rho, cmap="viridis")
-    ax.scatter(positions[0,:,0], positions[0,:,1], color="red", s=1)
+    positions = np.random.random(size=(no_particles,2))*L
+    velocities = np.random.normal(size=(no_particles,2))*v0
+
+    acc = accelerations(positions, masses, L, h, c_s, rho0)
+    print(acc)
+    # Plot densities
+    x,y,density_grid = get_density_grid(positions, [1]*no_particles, L, h)
+    particle_densities = get_densities(positions, [1]*no_particles, L, h)
+    fig, (ax,ax2) = plt.subplots(ncols=2)
+    pcol = ax.pcolor(x,y,density_grid, cmap="viridis", vmin=np.amin(density_grid), vmax=np.amax(density_grid))
+    ax2.scatter(positions[:,0], positions[:,1], c=particle_densities, vmin=np.amin(density_grid), vmax=np.amax(density_grid) , s=5)
+    fig.colorbar(pcol)
+
+    x,y,pressure_grid = get_pressure_grid(density_grid,c_s,rho0)
+    particle_pressures = get_pressures(particle_densities, c_s, rho0)
+    fig, (ax,ax2) = plt.subplots(ncols=2)
+    ax.pcolor(x,y,pressure_grid, cmap="viridis", vmin=np.amin(pressure_grid), vmax=np.amax(pressure_grid))
+    ax2.scatter(positions[:,0], positions[:,1], c=particle_pressures, cmap="viridis", vmin=np.amin(pressure_grid), vmax=np.amax(pressure_grid), s=5)
     plt.show()
-    animate_particles(positions, L, no_frames=100)
+    
+    
+    print("integrating...")
+    pos, vel = integrate(positions, velocities, masses, L, h, c_s, rho0, dt, no_steps)
+    print("animating...")
+    animate_particles(pos, L, no_steps)
     
 
