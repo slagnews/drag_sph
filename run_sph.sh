806531aec9a95fc141b8815d0fe6a25ae3061d0e
mlenes <mats.lenes@gmail.com>
2025-05-28 19:30:59 +0200

    Added particle sorter for cache locality

--- run_sph.sh
+++ run_sph.sh
@@ -1,13 +1,14 @@
 #! /bin/bash
 
-no_particles=2
+no_particles=3
 no_steps=10
 Lx=10
 Ly=10
+rho0=1000
 h=0.5
 v0=10
 dt=0.01
 c_s=0.01
 gamma_index=7
 
-./sph $no_particles $no_steps $Lx $Ly $h $v0 $dt $c_s $gamma_index
+./sph $no_particles $no_steps $Lx $Ly $rho0 $h $v0 $dt $c_s $gamma_index
--- sph.cpp
+++ sph.cpp
@@ -2,137 +2,68 @@
 #include <vector>
 #include <array>
 #include <cmath>
+#include <algorithm>
+#include <numeric>
 
-struct Vec2 {
-	double x, y;
-
-	Vec2 operator*(double coef) const {
-		return {x*coef, y*coef};
-	}
-
-	Vec2 operator*(const Vec2& other) const {
-		return {x*other.x, y*other.y};
-	}
-
-	Vec2 operator+(double coef) const {
-		return {x+coef, y+coef};
-	}
-
-	Vec2 operator+(const Vec2& other) const {
-		return {x+other.x, y+other.y};
-	}
-
-	Vec2 operator-(double coef) const {
-		return {x-coef, y-coef};
-	}
-
-	Vec2 operator-(const Vec2& other) const {
-		return {x-other.x, y-other.y};
-	}
-
-	Vec2& operator=(const Vec2& other) {
-		x = other.x;
-		y = other.y;
-		return *this;
-	}
-
-	Vec2& operator+=(double coef) {
-		x += coef;
-		y += coef;
-		return *this;
-	}
-
-	Vec2& operator+=(const Vec2& other) {
-		x += other.x;
-		y += other.y;
-		return *this;
-	}
-
-	Vec2& operator-=(double coef) {
-		x -= coef;
-		y -= coef;
-		return *this;
-	}
-
-	Vec2& operator-=(const Vec2& other) {
-		x -= other.x;
-		y -= other.y;
-		return *this;
-	}
-
-	double norm() const {
-		return sqrt(pow(x, 2) + pow(y, 2));
-	}
-
-	double dist(const Vec2& other) const {
-		return sqrt(pow(x-other.x, 2) + pow(y-other.y, 2));
-	}
-};
-
-
-enum class ParticleType {
-	flow, in, out, ghost
-};
-
-struct Particle {
-	Vec2 pos, vel, acc;
-	double mass, rho, pressure;
-	ParticleType type;
-};
-
-class SPHSimulation {
-	public:
-		double c_s;
-		double gamma_index;
-		double rho0;
-		std::vector<Particle> particles;
-
-	void compute_pressure() {
-
-	}
+struct ParticleList {
+	std::vector<double> pos_x;
+	std::vector<double> pos_y;
+	std::vector<double> mass;
+	std::vector<double> rho;
+	std::vector<double> pressure;
+	std::vector<int> cell_idx;
 };
 
 
 // Declare simulation parameters
 double Lx, Ly, h, v0, dt, c_s, gamma_index, rc, rho0;
-int no_particles, nCells, no_steps;
+int no_particles, no_cells, no_steps;
 std::array<int, 2> nc;
 
-// Vector of the particle positions
-std::vector<std::array<double, 2>> positions;
-
-// Linked list data
-std::vector<int> head;
-std::vector<int> lscl;
-
-const int EMPTY = -1;
-
 // Helper functions
-std::array<int, 2> get_cell_index(const std::array<double, 2>& pos) {
-	return { int(pos[0] / rc), int(pos[1] / rc) };
+std::array<int, 2> get_cell_index(double x, double y) {
+	return { int(x / rc), int(y / rc) };
 }
 
 int vec_to_scalar_index(int cx, int cy) {
 	return cx * nc[1] + cy;
 }
 
-// Constructing the linked list for departmentalization
-void construct_linked_list() {
-	head.assign(nCells, EMPTY);
-	lscl.assign(no_particles, EMPTY);
+// Assign the corresponding cell to each particle
+void assign_cell(ParticleList& pl) {
 
 	for (int i = 0; i < no_particles; ++i) {
-		auto cell = get_cell_index(positions[i]);
+		auto cell = get_cell_index(pl.pos_x[i], pl.pos_y[i]);
 		int cx = cell[0];
 		int cy = cell[1];
 		int c = vec_to_scalar_index(cx, cy);
-
-		lscl[i] = head[c];
-		head[c] = i;
+		pl.cell_idx[i] = c;
     }
 }
 
-double kernel(Vec2 r_vec, double h) {
+std::vector<int> sorter(int no_particles, const ParticleList& pl) {
+	std::vector<int> indices(no_particles);
+	std::iota(indices.begin(), indices.end(), 0);
+	
+	std::stable_sort(indices.begin(), indices.end(),
+	 [&](int a, int b) {
+	 	return pl.cell_idx[a] < pl.cell_idx[b];
+	 });
+	 return indices;
+}
+
+template<typename T>
+std::vector<T> reorder(const std::vector<T>& input, const std::vector<int>& indices) {
+	std::vector<T> output(indices.size());
+	for (size_t i = 0; i < indices.size(); ++i) {
+		output[i] = input[indices[i]];
+	}
+	return output;
+}
+
+
+/*
+double kernel(std::array<double, 2> r_vec, double h) {
 	double r = r_vec.norm();
 	double q = r/h;
 	double sigma = (15/(14*M_PI*pow(h, 2)));
@@ -152,7 +83,7 @@ double kernel(Vec2 r_vec, double h) {
 	}
 }
 
-double deriv_kernel(Vec2 r_vec, double h) {
+double deriv_kernel(std::array<double, 2> r_vec, double h) {
 	double r = r_vec.norm();
 	double q = r/h;
 	double sigma = (15/(14*M_PI*pow(h, 3)));
@@ -171,6 +102,7 @@ double deriv_kernel(Vec2 r_vec, double h) {
 		return 0;
 	}
 }
+*/
 
 double cole_pressure(double rho, double c_s, double rho0, double gamma_index) {
 	double B = pow(c_s, 2)*rho0/gamma_index;
@@ -184,41 +116,35 @@ int main(int argc, char *argv[]) {
 		no_steps = atoi(argv[2]);
 		Lx = atof(argv[3]);
 		Ly = atof(argv[4]);
-		h = atof(argv[5]);
-		v0 = atof(argv[6]);
-		dt = atof(argv[7]);
-		c_s = atof(argv[8]);
-		gamma_index = atof(argv[9]);
+		rho0 = atof(argv[5]);
+		h = atof(argv[6]);
+		v0 = atof(argv[7]);
+		dt = atof(argv[8]);
+		c_s = atof(argv[9]);
+		gamma_index = atof(argv[10]);
 
 		rc = 2*h;
 		nc = {int(Lx / rc), int(Ly / rc)};
-		nCells = nc[0] * nc[1];
+		no_cells = nc[0] * nc[1];
 	}
+	
+	double particle_mass = Lx*Ly*rho0/2;
 
-	positions = {{0.5, 0.5}, {0.6, 0.6}, {2.3, 1.1}, {0.4, 0.7}};
-
-	construct_linked_list();
-
-	SPHSimulation sim;
-	Particle p1, p2;
-
-	p1.pos = {1, 1};
-	p1.vel = {1, 0};
-	p1.acc = {0, 0};
-	p1.mass = 1.0;
-	p1.rho = 0;
-	p1.pressure = 0;
-	p1.type = ParticleType::flow;
-	sim.particles.push_back(p1);
-
-	p2.pos = {2, 1};
-	p2.vel = {-1, 0};
-	p2.acc = {0, 0};
-	p2.mass = 1.0;
-	p2.rho = 0;
-	p2.pressure = 0;
-	p2.type = ParticleType::flow;
-	sim.particles.push_back(p2);
+	ParticleList pl = {{1.2, 8, 5.5}, {4, 4, 6.2}, {particle_mass, particle_mass, particle_mass}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+	
+	std::cout << pl.pos_x[0] << std::endl;
+	std::cout << pl.pos_x[1] << std::endl;
+	std::cout << pl.pos_x[2] << std::endl << std::endl;
+	
+	assign_cell(pl);
+	auto indices = sorter(no_particles, pl);
+	pl.pos_x = reorder(pl.pos_x, indices);
+	
+	std::cout << pl.pos_x[0] << std::endl;
+	std::cout << pl.pos_x[1] << std::endl;
+	std::cout << pl.pos_x[2] << std::endl;
+	
+	
 
 	return 0;
 }
