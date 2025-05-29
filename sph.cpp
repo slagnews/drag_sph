4006b30a69dc4e26a100c798edaf4c83c7291460
mlenes <mats.lenes@gmail.com>
2025-05-29 08:58:54 +0200

    Refactored cpp code so that functions are members of classes

--- sph.cpp
+++ sph.cpp
@@ -5,119 +5,120 @@
 #include <algorithm>
 #include <numeric>
 
-struct ParticleList {
-	std::vector<double> pos_x;
-	std::vector<double> pos_y;
-	std::vector<double> mass;
-	std::vector<double> rho;
-	std::vector<double> pressure;
-	std::vector<int> cell_idx;
+struct SimulationParams {
+	double Lx, Ly, h, rc, v0, dt, c_s, gamma_index, rho0;
+	int no_particles, no_cells, no_steps;
+	std::array<int, 2> nc;
+	
+	SimulationParams(int no_steps, double Lx, double Ly, double rho0, double h, double v0, double dt, double c_s, double gamma_index)
+		: no_steps(no_steps),
+		  Lx(Lx),
+		  Ly(Ly),
+		  rho0(rho0),
+		  h(h),
+		  v0(v0),
+		  dt(dt),
+		  c_s(c_s),
+		  gamma_index(gamma_index)
+	{
+		rc = 2*h;
+		nc = {int(Lx/rc), int(Ly/rc)};
+		no_cells = nc[0] * nc[1];
+	}
 };
 
-
-// Declare simulation parameters
-double Lx, Ly, h, v0, dt, c_s, gamma_index, rc, rho0;
-int no_particles, no_cells, no_steps;
-std::array<int, 2> nc;
-
-// Helper functions
-std::array<int, 2> get_cell_index(double x, double y) {
-	return { int(x / rc), int(y / rc) };
-}
-
-int vec_to_scalar_index(int cx, int cy) {
-	return cx * nc[1] + cy;
-}
-
-// Assign the corresponding cell to each particle
-void assign_cell(ParticleList& pl) {
-
-	for (int i = 0; i < no_particles; ++i) {
-		auto cell = get_cell_index(pl.pos_x[i], pl.pos_y[i]);
-		int cx = cell[0];
-		int cy = cell[1];
-		int c = vec_to_scalar_index(cx, cy);
-		pl.cell_idx[i] = c;
-    }
-}
-
-std::vector<int> sorter(int no_particles, const ParticleList& pl) {
-	std::vector<int> indices(no_particles);
-	std::iota(indices.begin(), indices.end(), 0);
+struct ParticleList {
+	// Particle information
+	int no_particles;
+	const SimulationParams& params;
 	
-	std::stable_sort(indices.begin(), indices.end(),
-	 [&](int a, int b) {
-	 	return pl.cell_idx[a] < pl.cell_idx[b];
-	 });
-	 return indices;
-}
-
-template<typename T>
-std::vector<T> reorder(const std::vector<T>& input, const std::vector<int>& indices) {
-	std::vector<T> output(indices.size());
-	for (size_t i = 0; i < indices.size(); ++i) {
-		output[i] = input[indices[i]];
+	// Particle information vectors
+	std::vector<double> pos_x, pos_y, mass, rho, pressure;
+	std::vector<int> cell_idx, indices, cell_counts, cell_start;
+	
+	// Constructor
+	ParticleList(int no_particles,  const SimulationParams& p)
+		: params(p),
+		  no_particles(no_particles),
+		  pos_x(no_particles),
+		  pos_y(no_particles),
+		  mass(no_particles),
+		  rho(no_particles),
+		  pressure(no_particles),
+		  cell_idx(no_particles),
+		  indices(no_particles),
+		  cell_counts(p.no_cells),
+		  cell_start(p.no_cells+1) {}
+		  
+	// Helper functions
+	std::array<int, 2> get_cell_index(double x, double y) {
+		return { int(x / params.rc), int(y / params.rc) };
 	}
-	return output;
-}
 
-std::vector<int> get_cell_start(int no_particles, int no_cells, const std::vector<int>& indices, const std::vector<int>& cell_idx) {
-	std::vector<int> cell_counts(no_cells, 0);
-	
-	for (int i=0; i<no_particles; i++) {
-		int cell = cell_idx[indices[i]];
-		cell_counts[cell]++;
+	int vec_to_scalar_index(int cx, int cy) {
+		return cx * params.nc[1] + cy;
 	}
 	
-	std::vector<int> cell_start(no_cells + 1, 0);
-	for (int c=0; c < no_cells; c++) {
-		cell_start[c+1] = cell_start[c] + cell_counts[c];
+	void assign_cell() {
+	// Assign the corresponding cell to each particle
+		for (int i = 0; i < no_particles; ++i) {
+			auto cell = get_cell_index(pos_x[i], pos_y[i]);
+			int cx = cell[0];
+			int cy = cell[1];
+			int c = vec_to_scalar_index(cx, cy);
+			cell_idx[i] = c;
+		}
 	}
-	return cell_start;
-}
 
-
-/*
-double kernel(std::array<double, 2> r_vec, double h) {
-	double r = r_vec.norm();
-	double q = r/h;
-	double sigma = (15/(14*M_PI*pow(h, 2)));
-
-	if (q > 2) {
-		return 0;
-
-	} else if ((q > 1) and (q <= 2)) {
-		return sigma*pow(2-q, 3);
-
-	} else if ((q >= 0) and (q <= 1)) {
-		return sigma*(pow(2-q, 3)-4*pow(1-q, 3));
-
-	} else {
-		std::cout << "q smaller than 0" << std::endl;
-		return 0;
+	void get_sorter() {
+	// Get the indices needed to sort particle vectors
+		std::iota(indices.begin(), indices.end(), 0);
+		
+		std::stable_sort(indices.begin(), indices.end(),
+		 [&](int a, int b) {
+		 	return cell_idx[a] < cell_idx[b];
+		 });
 	}
-}
-
-double deriv_kernel(std::array<double, 2> r_vec, double h) {
-	double r = r_vec.norm();
-	double q = r/h;
-	double sigma = (15/(14*M_PI*pow(h, 3)));
-
-	if (q > 2) {
-		return 0;
-
-	} else if ((q > 1) and (q <= 2)) {
-		return sigma*(-3*pow(2-q, 2));
-
-	} else if ((q >= 0) and (q <= 1)) {
-		return sigma*(-3*pow(2-q, 2)+12*pow(1-q, 2));
-
-	} else {
-		std::cout << "q smaller than 0" << std::endl;
-		return 0;
+	
+	void get_cell_start() {
+		std::fill(cell_counts.begin(), cell_counts.end(), 0);
+		
+		for (int i=0; i<no_particles; i++) {
+			int cell = cell_idx[indices[i]];
+			cell_counts[cell]++;
+		}
+		
+		std::fill(cell_start.begin(), cell_start.end(), 0);
+		for (int c=0; c < params.no_cells; c++) {
+			cell_start[c+1] = cell_start[c] + cell_counts[c];
+		}
 	}
-}
-*/
+
+	template<typename T>
+	std::vector<T> reorder(const std::vector<T>& input, const std::vector<int>& indices) {
+		std::vector<T> output(indices.size());
+		for (size_t i = 0; i < indices.size(); ++i) {
+			output[i] = input[indices[i]];
+		}
+		return output;
+	}
+
+
+	void compart() {
+	// Does all of the above steps, call this after every step to (re)compartmentalize the data.
+		assign_cell();
+		
+		get_sorter();
+		
+		get_cell_start();
+		
+		pos_x = reorder(pos_x, indices);
+		pos_y = reorder(pos_y, indices);
+		mass = reorder(mass, indices);
+		rho = reorder(rho, indices);
+		pressure = reorder(pressure, indices);
+	}
+};
 
 double cole_pressure(double rho, double c_s, double rho0, double gamma_index) {
 	double B = pow(c_s, 2)*rho0/gamma_index;
@@ -126,42 +127,24 @@ double cole_pressure(double rho, double c_s, double rho0, double gamma_index) {
 
 
 int main(int argc, char *argv[]) {
-	if (argc >= 2) {
-		no_particles = atoi(argv[1]);
-		no_steps = atoi(argv[2]);
-		Lx = atof(argv[3]);
-		Ly = atof(argv[4]);
-		rho0 = atof(argv[5]);
-		h = atof(argv[6]);
-		v0 = atof(argv[7]);
-		dt = atof(argv[8]);
-		c_s = atof(argv[9]);
-		gamma_index = atof(argv[10]);
-
-		rc = 2*h;
-		nc = {int(Lx / rc), int(Ly / rc)};
-		no_cells = nc[0] * nc[1];
+	if (argc < 11) {
+		std::cerr << "Not enough arguments given" << std::endl;
+		return 1;
 	}
 	
-	double particle_mass = Lx*Ly*rho0/2;
-
-	ParticleList pl = {{1.2, 1.2, 5.5}, {8, 8, 6.2}, {particle_mass, particle_mass, particle_mass}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+	SimulationParams params(
+		atoi(argv[2]), // no_steps
+		atof(argv[3]), // Lx
+		atof(argv[4]), // Ly
+		atof(argv[5]), // rho0
+		atof(argv[6]), // h
+		atof(argv[7]), // v0
+		atof(argv[8]), // dt
+		atof(argv[9]), // c_s
+		atof(argv[10]) // gamma_index
+	);
 	
-	assign_cell(pl);
-	auto indices = sorter(no_particles, pl);
-	pl.pos_x = reorder(pl.pos_x, indices);
-	auto cell_start = get_cell_start(no_particles, no_cells, indices, pl.cell_idx);
+	ParticleList pl(atoi(argv[1]), params); // Where argv[1] = no_particles
 	
-	std::cout << pl.cell_idx[0] << std::endl;
-	std::cout << pl.cell_idx[1] << std::endl;
-	std::cout << pl.cell_idx[2] << std::endl;
-	
-	int start = cell_start[18];
-	int end = cell_start[18+1];
-	
-	for (int i=start; i<end; i++) {
-		std::cout << pl.pos_x[i] << std::endl;
-	}
-
 	return 0;
 }
