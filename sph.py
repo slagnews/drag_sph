03f25be31c5580da9aead2a88376d10688681514
slagnews <swinkelsn@gmail.com>
2025-06-01 20:11:34 +0200

    imlemented leapfrog integration and started on ghost particles

--- sph.py
+++ sph.py
@@ -64,7 +64,7 @@ def get_densities(relative_distances, masses, h):
     """
     return np.sum(masses*kernel(relative_distances, h), axis=1)
 
-def get_density_grid(positions, masses, L, h, grid_size=100):
+def get_density_grid(positions, masses, L, h, grid_size=100, is_ghost=None):
     """Calculates the densities on a grid
     Arguments:
         positions (np.ndarray): positions of all particles
@@ -77,21 +77,24 @@ def get_density_grid(positions, masses, L, h, grid_size=100):
         y (np.ndarray): 1D array of y coordinates of the grid.
         density (np.ndarray): 2D array of shape (grid_size, grid_size) containing the density values at each grid point.
     """
+    if not isinstance(is_ghost, np.ndarray):
+        is_ghost = np.zeros(len(positions), dtype=bool)
+
     # Generate grid
     x = np.linspace(0,L,grid_size)
     y = np.linspace(0,L,grid_size)
     X,Y = np.meshgrid(x,y)
 
     # Apply periodic boundary conditions to distances
-    dx = (X[:,:,None]-positions[:,0])
+    dx = (X[:, :, None] - positions[~is_ghost, 0])
     dx = (dx+L/2)%L - L/2
-    dy = (Y[:,:,None]-positions[:,1])
+    dy = (Y[:, :, None] - positions[~is_ghost, 1])
     dy = (dy+L/2)%L - L/2
 
     distances = np.sqrt(dx**2+dy**2)
 
     # Calculate density
-    density = np.sum(masses*kernel(distances, h), axis=2)
+    density = np.sum(masses[~is_ghost]*kernel(distances, h), axis=2)
     return x,y,density
 
 def ideal_pressure(rho, c_s, rho0, beta=0.07):
@@ -154,7 +157,7 @@ def central_object_forces(positions, L, central_object):
         return forces
 
 
-def integrate(initial_positions, initial_velocities, masses, L, h, c_s, rho0, dt, no_steps, central_object=None):
+def integrate(initial_positions, initial_velocities, masses, L, h, c_s, rho0, dt, no_steps, central_object=None, is_ghost=None):
     """Integrate the positions and velocities of particles in a 2D space.
     Arguments:
         initial_positions: 2D numpy array of shape (no_particles, 2) containing the initial x and y coordinates of the particles.
@@ -163,35 +166,55 @@ def integrate(initial_positions, initial_velocities, masses, L, h, c_s, rho0, dt
         dt: Time step for integration.
         no_steps: Number of time steps to integrate.
         central_object (dict): properties of an optional central object
+        is_ghost (np.ndarray): booleans indicating for each particle if it is a ghost
     Returns:
         positions: 3D numpy array of shape (no_steps, no_particles, 2) containing the x and y coordinates of the particles at each time step.
         velocities: 3D numpy array of shape (no_steps, no_particles, 2) containing the x and y velocities of the particles at each time step.
     """
+    if not isinstance(is_ghost, np.ndarray):
+        is_ghost = np.array([0]*len(initial_positions))
+    
     start = datetime.now()
 
     # Initialize data arrays
     no_particles = initial_positions.shape[0]
     positions = np.zeros((no_steps, no_particles, 2))
     velocities = np.zeros((no_steps, no_particles, 2))
+    forces = np.zeros((no_steps, no_particles, 2))
 
     # Set initial conditions
     positions[0] = initial_positions
     velocities[0] = initial_velocities
+    if central_object:
+        forces[0] = get_pressure_forces(positions[0], masses, L, h, c_s, rho0) + central_object_forces(positions[0], L, central_object)
+    else:
+        forces[0] = get_pressure_forces(positions[0], masses, L, h, c_s, rho0)
     
-    for i in range(1, no_steps):
-        positions[i] = positions[i-1] + velocities[i-1] * dt
-        positions[i] %= L
-
+    for i in range(no_steps-1):
+        positions[i+1] = positions[i] + velocities[i] * dt + 1/2*forces[i]/masses[:,None]*dt**2
+        positions[i+1,is_ghost] = positions[i,is_ghost]
+        positions[i+1] %= L
         if central_object:
-            velocities[i] = velocities[i-1] + dt*(get_pressure_forces(positions[i], masses, L, h, c_s, rho0)+central_object_forces(positions[i], L, central_object))/masses[:,None]
+            forces[i+1] = get_pressure_forces(positions[i+1], masses, L, h, c_s, rho0) + central_object_forces(positions[i+1], L, central_object)
         else:
-            velocities[i] = velocities[i-1] + dt*get_pressure_forces(positions[i], masses, L, h, c_s, rho0)/masses
-        ttg = (datetime.now()-start)/i * (no_steps-i)
+            forces[i+1] = get_pressure_forces(positions[i+1], masses, L, h, c_s, rho0)
+        velocities[i+1] = velocities[i] + 1/2*(forces[i] + forces[i+1])*dt
+        velocities[i+1,is_ghost] = velocities[i,is_ghost]
+        
+
+        
+        #if central_object:
+        #    #forces = get_pressure_forces(positions[i], masses, L, h, c_s, rho0)+central_object_forces(positions[i], L, central_object)
+        #    velocities[i+1] = velocities[i] + dt*(get_pressure_forces(positions[i], masses, L, h, c_s, rho0)+central_object_forces(positions[i], L, central_object))/masses[:,None]
+        #else:
+        #    #forces = get_pressure_forces(positions[i], masses, L, h, c_s, rho0)
+        #    velocities[i+1] = velocities[i] + dt*get_pressure_forces(positions[i], masses, L, h, c_s, rho0)/masses
+        ttg = (datetime.now()-start)/(i+1) * (no_steps-i-1)
         print(f"{(i+1)/no_steps*100:.0f}% done, time to go: {ttg}", end="\r")
     return positions, velocities
 
 
-def animate_particles(positions, masses, L, h, fps=30, central_object=None, file_name="animation.mp4"):
+def animate_particles(positions, masses, L, h, fps=30, central_object=None, file_name="animation.mp4", is_ghost=None):
     """Animates the positions of particles in a 2D space.
     Arguments:
         positions (np.ndarray): positions of all particles
@@ -211,24 +234,27 @@ def animate_particles(positions, masses, L, h, fps=30, central_object=None, file
     ax.set_xlabel("$x$")
     ax.set_ylabel("$y$")
     ax.set_aspect("equal")
-    scatter, = ax.plot(positions[0,:,0], positions[0,:,1], linestyle="None", marker="o", markersize=1, color="red")
-    x,y,rho = get_density_grid(positions[0], masses, L, h)
+    fluid_particles, = ax.plot(positions[0,~is_ghost,0], positions[0,~is_ghost,1], linestyle="None", marker="o", markersize=1, color="red")
+    ghost_particles, = ax.plot(positions[0,is_ghost,0], positions[0,is_ghost,1], linestyle="None", marker="o", markersize=1, color="white")
+    x,y,rho = get_density_grid(positions[0], masses, L, h, is_ghost=is_ghost)
     mesh = ax.pcolor(x,y,rho, cmap="viridis")
-    if object:
+    if central_object:
         theta = np.linspace(0,2*np.pi,100)
         ax.plot(L/2+central_object["radius"]*np.cos(theta),L/2+central_object["radius"]*np.sin(theta), color="white")
     start = datetime.now()
     def update(frame):
         """Update the scatter plot for each frame."""
         
-        scatter.set_xdata(positions[frame,:,0])
-        scatter.set_ydata(positions[frame,:,1])
-        x,y,rho = get_density_grid(positions[frame], masses, L, h)
+        fluid_particles.set_xdata(positions[frame,~is_ghost,0])
+        fluid_particles.set_ydata(positions[frame,~is_ghost,1])
+        ghost_particles.set_xdata(positions[frame,is_ghost,0])
+        ghost_particles.set_ydata(positions[frame,is_ghost,1])
+        x,y,rho = get_density_grid(positions[frame], masses, L, h, is_ghost=is_ghost)
         mesh.set_array(rho.ravel())
 
         ttg = (datetime.now() - start)/(frame+1) * (no_frames - frame)
         print(f"Frame {frame+1} of {no_frames} done, time to go: {ttg}", end="\r")
-        return scatter,mesh
+        return fluid_particles,ghost_particles,mesh
 
     ani = FuncAnimation(fig, update, frames=no_frames, interval=50)
     plt.show()
