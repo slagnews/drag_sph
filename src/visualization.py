68f23992f90593a27292aebec2e11cf882b589dd
slagnews <swinkelsn@gmail.com>
2025-06-02 17:52:57 +0200

    Added energy calculation

--- src/visualization.py
+++ src/visualization.py
@@ -79,14 +79,47 @@ def get_density_grid(pos_x, pos_y, Lx, Ly, masses, sigma, grid_size=100):
     density = np.sum(masses[0]*kernel(distances, sigma), axis=2)
     return x,y,density
 
-def animate(positions, params, masses):
+
+
+def get_velocity_grid(positions, velocities, masses, params, grid_size=100):
+    """Interpolated the velocities of particles on a grid
+    Arguments:
+        positions (np.ndarray): the positions of all particles
+        velocities (np.ndarray): velocities of all particles
+
+    """
+    _, distances = relative_positions(positions, L)
+    densities = get_densities(distances, masses, h)
+
+    x = np.linspace(0, L, grid_size)
+    y = np.linspace(0, L, grid_size)
+    X, Y = np.meshgrid(x, y)
+
+    # Differences between grid and particle positions
+    dx = (X[:, :, None] - positions[:, 0])
+    dx = (dx + L / 2) % L - L / 2
+    dy = (Y[:, :, None] - positions[:, 1])
+    dy = (dy + L / 2) % L - L / 2
+
+    distances = np.sqrt(dx**2 + dy**2)
+    W = kernel(distances, h)  # shape: (grid_size, grid_size, N)
+
+    # Broadcast and weight velocities: (N,2) → (grid, grid, N, 2)
+    velocity_contribs = (masses / densities)[:, None] * velocities  # shape (N, 2)
+    Vx = np.sum(W * velocity_contribs[:, 0], axis=2)
+    Vy = np.sum(W * velocity_contribs[:, 1], axis=2)
+    V = np.sqrt(Vx**2+Vy**2)
+
+    return X, Y, Vx, Vy, V
+
+def animate(positions, params, masses, filename="figures/anim.mp4", interval=100):
     fig, ax = plt.subplots()
     ax.set_xlim(0,float(params.Lx))
     ax.set_ylim(0,float(params.Ly))
     ax.set_aspect("equal")
 
     # Plot particles
-    particles, = ax.plot(positions[0,:,0], positions[0,:,1], linestyle="None", marker="o", color="red")
+    particles, = ax.plot(positions[0,:,0], positions[0,:,1], linestyle="None", marker=".", color="red")
 
     # Plot central object
     phi = np.linspace(0,2*np.pi,100)
@@ -103,6 +136,42 @@ def animate(positions, params, masses):
         density.set_array(rho)
         print(f"frame {frame+1} of {len(positions)} done!", end='\r')
 
-    anim = FuncAnimation(fig, update, len(positions))
-    anim.save("figures/cpp_anim.mp4")
+    anim = FuncAnimation(fig, update, len(positions), interval=interval)
+    anim.save(filename)
     plt.show()
+
+def calculate_energies(positions, velocities, params, masses):
+    # Total mass
+    M = np.sum(masses)
+
+    no_steps = len(positions)
+    E_bulk = np.zeros(no_steps)
+    E_rand = np.zeros(no_steps)
+    E_kinetic = np.zeros(no_steps)
+    E_total = np.zeros(no_steps)
+    E_object = np.zeros(no_steps)
+
+    for t in range(no_steps):
+        v = velocities[t]
+        
+        momentum = np.sum(masses[:, None] * v, axis=0)
+        v_mean = momentum / M
+
+        # Energy from bulk flow
+        E_bulk[t] = 0.5 * M * np.dot(v_mean, v_mean)
+
+        # Energy from object potential
+        distances = np.sqrt((positions[t][:,0]-params.Lx/2)**2+(positions[t][:,1]-params.Ly/2)**2)
+        E_object[t] = np.sum(masses*params.max_force*params.boundary_width*np.log(np.exp((params.central_radius-distances)/params.boundary_width)+1))
+
+        # Total kinetic energy
+        kinetic = 0.5 * masses * np.sum(v**2, axis=1)
+        E_kinetic[t] = np.sum(kinetic)
+        
+        # Energy from random motion
+        E_rand[t] = E_kinetic[t] - E_bulk[t]
+
+        # Total energy including potential
+        E_total[t] = E_kinetic[t] + E_object[t]
+
+    return E_total, E_bulk, E_rand, E_object
