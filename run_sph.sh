9cdfc8e6bf47aef63bc9b77035630e0985b47c43
mlenes <mats.lenes@gmail.com>
2025-06-02 09:50:55 +0200

    settings for sph simulation

--- run_sph.sh
+++ run_sph.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
-no_particles=3
+res=0.1
 no_steps=10
-Lx=10
-Ly=10
+Lx=5
+Ly=5
 rho0=1000
-h=0.5
+kappa=1.2
 v0=10
 dt=0.01
 c_s=0.01
 gamma_index=7
 
-./sph $no_particles $no_steps $Lx $Ly $rho0 $h $v0 $dt $c_s $gamma_index
+./sph $res $no_steps $Lx $Ly $rho0 $kappa $v0 $dt $c_s $gamma_index
